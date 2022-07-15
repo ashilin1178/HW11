6a43845577539906bb4cdd88760b2f450b4c90e8
Anton <ashilin1178@gmail.com>
2022-07-16 00:26:23 +0800

    list all complited

--- app.py
+++ app.py
@@ -10,23 +10,42 @@ candidates = utils.load_candidates_from_json(file_adress)
 
 @app.route('/')
 def get_list_all():
+    """
+    вывод всех кандидатов на одну страницу
+    :return:
+    """
     return render_template('list.html', candidates=candidates)
 
 
 @app.route('/candidate/<int:id>')
 def get_candidate_card(id):
+    """
+    выводит кандидата по id
+    :param id:
+    :return:
+    """
     candidate = utils.get_candidate(candidates, id)
     return render_template('card.html', candidate=candidate)
 
 
 @app.route('/candidate/<candidate_name>')
 def get_candidate_name(candidate_name):
+    """
+    выводит кандидатов по совпадению имени или фамилии
+    :param candidate_name:
+    :return:
+    """
     candidate = utils.get_candidate_by_name(candidates, candidate_name)
     return render_template('search.html', candidate=candidate)
 
 
 @app.route('/skill/<skill>')
 def get_candidate_skill(skill):
+    """
+    выводит кандидатов с навыками
+    :param skill:
+    :return:
+    """
     candidate = utils.get_candidate_by_skill(candidates, skill)
     return render_template('skill.html', candidate=candidate, skill=skill)
 
